c4f7d71f9664d42ab7206e04df49d23e7d2fd218
Andrew Payne <46719251+payneandrew@users.noreply.github.com>
2023-10-20 13:08:28 -0400

    Setup

---
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { USStates } from 'src/app/models/states';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.css']
+})
+export class LandingPageComponent {
+  states = USStates
+}
